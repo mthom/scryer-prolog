76bae21ff66f270292a0563823300b596e24008b
Mark Thom <markjordanthom@gmail.com>
2018-05-03 11:52:33 -0600

    correct keysort type error (re: #34)

--- src/prolog/machine/machine_state_impl.rs
+++ src/prolog/machine/machine_state_impl.rs
@@ -1742,7 +1742,7 @@ impl MachineState {
                     _ => Err(self.error_form(self.type_error(ValidType::Pair,
                                                              self.heap[s].as_addr(s))))
                 },
-            a => Err(self.error_form(self.type_error(ValidType::Callable, a)))
+            a => Err(self.error_form(self.type_error(ValidType::Pair, a)))
         }
     }
 
