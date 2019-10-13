249b613e3659078444621947cc3120c0eea114f9
Mark Thom <markjordanthom@gmail.com>
2019-10-12 19:57:08 -0600

    remove nondeterminism from put_atts as a result of lists.pl reversion

--- src/prolog/lib/atts.pl
+++ src/prolog/lib/atts.pl
@@ -108,7 +108,7 @@ get_attrs_var_check -->
 put_attrs(Name/Arity) -->
     put_attr(Name, Arity),
     { numbervars([Var, Attr], 0, _) },
-    [(put_atts(Var, Attr) :- lists:maplist(put_atts(Var), Attr))].
+    [(put_atts(Var, Attr) :- lists:maplist(put_atts(Var), Attr), !)].
 put_attrs((Name/Arity, Atts)) -->
     { nonvar(Atts) },
     put_attr(Name, Arity),
