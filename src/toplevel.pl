687547ec76d0291f4a9c2dca9bb9bf5611c59773
notoria <notoria@users.noreply.github.com>
2021-03-23 18:13:55 +0100

    Add a Prolog printer for the top-level
    Also include @triska, @mthom feedbacks.

--- src/toplevel.pl
+++ src/toplevel.pl
@@ -9,15 +9,17 @@
 ]).
 :- use_module(library(dcgs)).
 :- use_module(library(files), [file_exists/1]).
-:- use_module(library(format), [format/2]).
+:- use_module(library(format), [format/2, format_//2]).
 :- use_module(library(iso_ext), [setup_call_cleanup/3]).
 :- use_module(library(lists), [
     append/2,
     append/3,
+    length/2,
     maplist/2,
     maplist/3,
     member/2,
-    reverse/2
+    reverse/2,
+    select/3
 ]).
 
 :- use_module(library('$project_atts')).
@@ -328,7 +330,8 @@ print_goal(Terms, AllEqs, MaxDepth, Cs) :-
 
 print_goal_(AllEqs, MaxDepth, Append, Term, Cs) :-
     Settings = [variable_names(AllEqs), max_depth(MaxDepth)],
-    write_term_to_chars(Term, Settings, Cs0), % Not good enough for REPL.
+    % write_term_to_chars(Term, Settings, Cs0), % Not good enough for REPL.
+    phrase(print_(Term, Settings, [], _, _), Cs0),
     append(Cs0, Append, Cs).
 
 :- meta_predicate list_filter(1, ?, ?).
@@ -414,4 +417,416 @@ eq_member(X, [Y|Ls]) :-
         X == Y
     ;   X == Y
     ;   eq_member(X, Ls)
+    ).
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  Prolog printer.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+chars(V) :- var(V), !, false.
+chars([]).
+chars([C|Cs]) :- atom(C), atom_length(C, 1), chars(Cs).
+
+spec_arity(fx, 1).
+spec_arity(fy, 1).
+spec_arity(xf, 1).
+spec_arity(yf, 1).
+spec_arity(xfx, 2).
+spec_arity(xfy, 2).
+spec_arity(yfx, 2).
+
+symbolic_control('\a', "\\a").
+symbolic_control('\b', "\\b").
+symbolic_control('\f', "\\f").
+symbolic_control('\n', "\\n").
+symbolic_control('\r', "\\f").
+symbolic_control('\t', "\\t").
+symbolic_control('\v', "\\v").
+% symbolic_control('\x0\', "\x0\"). % '.
+
+print_double_quoted([]) --> [].
+print_double_quoted([C|Cs]) -->
+    (   { symbolic_control(C, Cs0) } ->
+        Cs0
+    % ;   { write_term_to_chars(C, [], Cs0) }, Cs0
+    ;   format_("~w", [C])
+    ),
+    print_double_quoted(Cs).
+
+print_quoted([]) --> [].
+print_quoted([C|Cs]) -->
+    (   { C = '''' } ->
+        "''"
+    ;   { symbolic_control(C, Cs0) } ->
+        Cs0
+    % ;   { write_term_to_chars(C, [], Cs0) }, Cs0
+    ;   format_("~w", [C])
+    ),
+    print_quoted(Cs).
+
+print_args([A|As], Settings, Ps) --> print_args(As, A, Settings, Ps).
+
+print_args([], A0, Settings, Ps) -->
+    (   { nonvar(A0),
+          functor(A0, F, N),
+          N >= 1, N =< 2,
+          current_op(Pri, S, F),
+          spec_arity(S, N),
+          Pri > 1000 } ->
+        "(", print_(A0, Settings, Ps, '(', _), ")"
+    ;   print_(A0, Settings, Ps, _, _)
+    ).
+print_args([A1|As], A0, Settings, Ps) -->
+    (   { nonvar(A0),
+          functor(A0, F, N),
+          N >= 1, N =< 2,
+          current_op(Pri, S, F),
+          spec_arity(S, N),
+          Pri > 1000 } ->
+        "(", print_(A0, Settings, Ps, '(', _), ")"
+    ;   print_(A0, Settings, Ps, _, _)
+    ),
+    ",", print_args(As, A1, Settings, Ps).
+
+print_list(List, Settings, Ps, MaxDepth0) -->
+    % { format("List: ~q ~q ~q\n", [Settings, Ps, MaxDepth0]) },
+    {   (   var(MaxDepth0) ->
+            (   member(max_depth(MaxDepth0), Settings) ->
+                true
+            ;   MaxDepth0 = 0 % Infinite
+            )
+        ;   true
+        )  ,
+        (   MaxDepth0 == none ->
+            true
+        ;   MaxDepth0 > 1 ->
+            MaxDepth is MaxDepth0 - 1
+        ;   MaxDepth0 =:= 1 ->
+            MaxDepth = none
+        ;   MaxDepth0 =:= 0 ->
+            MaxDepth = 0
+        )
+    },
+    (   { MaxDepth0 \== none } ->
+        (   { [L0|Ls] = List } ->
+            (   { nonvar(L0),
+                  functor(L0, F, N),
+                  N >= 1, N =< 2,
+                  current_op(Pri, S, F),
+                  spec_arity(S, N),
+                  Pri > 1000 } ->
+                { Parentheses = true, C0 = '(' },
+                "("
+            ;   { Parentheses = false }
+            ),
+            (   { var(Ls) } ->
+                print_(L0, Settings, Ps, C0, _),
+                (   { Parentheses } ->
+                    ")"
+                ;   []
+                ),
+                "|", print_(Ls, Settings, Ps, '|', _)
+            ;   { Ls = [] } ->
+                print_(L0, Settings, Ps, C0, _),
+                (   { Parentheses } ->
+                    ")"
+                ;   []
+                )
+            ;   { Ls = [_|_] } ->
+                print_(L0, Settings, Ps, C0, _),
+                (   { Parentheses } ->
+                    ")"
+                ;   []
+                ),
+                (   { eq_member(Ls, Ps) } ->
+                    (   { member(variable_names(Eqs), Settings) } ->
+                        (   { member(Name = V, Eqs), V == Ls } ->
+                            { atom_chars(Name, Cs) }, "|", Cs
+                        ;   { true } -> "_0"
+                        ;   { throw(error(instantiation_error, print_//5)) }
+                        )
+                    ;   { true } -> "_1"
+                    ;   { throw(error(instantiation_error, print_//5)) }
+                    )
+                ;   ",", print_list(Ls, Settings, Ps, MaxDepth)
+                )
+            ;   print_(L0, Settings, Ps, C0, _),
+                (   { Parentheses } ->
+                    ")"
+                ;   []
+                ),
+                "|", print_(Ls, Settings, Ps, C0, _)
+            )
+        ;   { [] = List } ->
+            print_(List, Settings, Ps, _, _)
+        ;   { throw(error(is_not_list(List), print_list//5)) }
+        )
+    ;   "..."
+    ).
+
+print_(Var, Settings, _Ps, L0, L) -->
+    { var(Var) }, !,
+    % { format("Variable: ~q\n", [Settings]) },
+    (   { member(variable_names(Eqs), Settings) } ->
+        (   { member(Name = V, Eqs), V == Var } ->
+            { atom_chars(Name, Cs), list_last(Cs, L) },
+            (   { nonvar(L0), char_type(L0, alnum) } ->
+                " "
+            ;   []
+            ),
+            Cs
+        ;   { true } ->
+            { L = '2' },
+            "_2"
+        ;   { throw(error(instantiation_error, print_//5)) }
+        )
+    ;   { true } ->
+        { L = '3' },
+        "_3"
+    ;   { throw(error(instantiation_error, print_//5)) }
+    ).
+print_(Number, _Settings, _Ps, L0, L) -->
+    { number(Number) ; rational(Number) }, !,
+    % { format("Number: ~q ~q\n", [Number, _Settings]) },
+    { number_chars(Number, Cs), list_last(Cs, L) },
+    (   { nonvar(L0),
+        ( char_type(L0, alnum)
+        ; Number < 0, char_type(L0, graphic_token)
+        ) } ->
+        " "
+    ;   []
+    ),
+    Cs.
+print_(Atom, _Settings, _Ps, L0, L) -->
+    { atom(Atom) }, !,
+    % { format("Atom: ~q ~q ~q ~q\n", [Atom, _Settings, _Ps, L0]) },
+    { atom_chars(Atom, Cs) },
+    (   { Cs = [] } ->
+        { member(Atom, ['']) } ->
+        { L = '''' },
+        "'", print_quoted(Cs), "'"
+    ;   { list_last(Cs, L1) },
+        (   { member(Atom, [!, ;, [], {}]) } ->
+            { L = L1 },
+            Cs
+        % ;   { Atom == (\) } -> % Not nice.
+        %     { L = L1 },
+        %     "\\"
+        ;   { member(Atom, [.]) } ->
+            (   { nonvar(L0),
+                  ( char_type(L0, graphic_token)
+                  ; char_type(L0, whitespace)
+                  ) } ->
+                { L = '''' },
+                "'.'"
+            ;   { L = L1 },
+                "."
+            )
+        ;   { [C|_] = Cs, char_type(C, numeric) } ->
+            % Quote is always required.
+            { L = '''' },
+            "'", print_quoted(Cs), "'"
+        % ;   { member(C, Cs), char_type(C, meta), C \= (\) } ->
+        %     { L = '''' },
+        %     "'", print_quoted(Cs), "'"
+        ;   { member(C, Cs), member(T, [solo, whitespace]), char_type(C, T) } ->
+            % Quote is always required.
+            { L = '''' },
+            "'", print_quoted(Cs), "'"
+        ;   { setof(
+                T,
+                C^(
+                    lists:member(C, Cs),
+                    lists:member(T, [alnum, graphic_token]),
+                    charsio:char_type(C, T)
+                ),
+                Ts
+              ),
+              length(Ts, N),
+              N > 1 } ->
+            % There is a mixture.
+            { L = '''' },
+            "'", print_quoted(Cs), "'"
+        ;   { member(C, Cs),
+              \+ ( member(T, [alnum, graphic_token]), char_type(C, T)) } ->
+            % There is an unknown character.
+            { L = '''' },
+            "'", print_quoted(Cs), "'"
+        ;   { L = L1 },
+            (   { nonvar(L0), [C|_] = Cs, member(T, [alnum, graphic_token]),
+                  char_type(C, T), char_type(L0, T) } ->
+                " "
+            ;   []
+            ),
+            Cs
+        )
+    ).
+print_(Compound, Settings0, Ps0, L0, L) -->
+    { Compound =.. [F|Args] }, !,
+    % { format("Compound: ~q ~q\n", [Compound, Settings]) },
+    { ( select(max_depth(MaxDepth0), Settings0, Settings1) ->
+        (   MaxDepth0 == none ->
+            Settings = Settings0
+        ;   MaxDepth0 > 1 ->
+            MaxDepth is MaxDepth0 - 1,
+            Settings = [max_depth(MaxDepth)|Settings1]
+        ;   MaxDepth0 =:= 1 ->
+            Settings = [max_depth(none)|Settings1]
+        ;   MaxDepth0 =:= 0 ->
+            Settings = [max_depth(0)|Settings1],
+            MaxDepth = 0
+        )
+      ; Settings = Settings0
+      )
+    },
+    (   { MaxDepth0 \== none } ->
+        (   { eq_member(Compound, Ps0) } ->
+            (   { member(variable_names(Eqs), Settings) } ->
+                (   { member(Name = V, Eqs), V == Compound } ->
+                    { atom_chars(Name, Cs), list_last(Cs, L) },
+                    (   { nonvar(L0), char_type(L0, alnum) } ->
+                        " "
+                    ;   []
+                    ),
+                    Cs
+                ;   { true } ->
+                    { L = '4' },
+                    "_4"
+                ;   { throw(error(instantiation_error, print_//5)) }
+                )
+            ;   { true } ->
+                { L = '5' },
+                "_5"
+            ;   { throw(error(instantiation_error, print_//5)) }
+            )
+        ;   { length(Args, N), Ps = [Compound|Ps0] },
+            % TODO: What happens if for example fx and xf is defined?
+            (   { N =:= 1, current_op(P, Spec, F), spec_arity(Spec, N) } ->
+                { [A] = Args, ( nonvar(A), functor(A, F1, N1) -> true ; true ) },
+                (   { member(Spec, [fx, fy]) } ->
+                    (   { number(A), A > 0, F = (-) } ->
+                        { L = ')' },
+                        print_(F, Settings, Ps, L0, _),
+                        " (", print_(A, Settings, Ps, '(', _), ")"
+                    ;   { atom(A), current_op(_, _, F1) } ->
+                        { L = ')' },
+                        print_(F, Settings, Ps, L0, _),
+                        " (", print_(A, Settings, Ps, '(', _), ")"
+                    ;   { compound(A), % N > 0,
+                          N1 >= 1, N1 =< 2,
+                          current_op(P1, S1, F1),
+                          spec_arity(S1, N1),
+                          ( Spec = fy, P < P1 -> true
+                          ; Spec = fx, P =< P1
+                          ) } ->
+                        { L = ')' },
+                        print_(F, Settings, Ps, L0, _),
+                        " (", print_(A, Settings, Ps, '(', _), ")"
+                    ;   print_(F, Settings, Ps, L0, L1),
+                        print_(A, Settings, Ps, L1, L)
+                    )
+                ;   { member(Spec, [xf, yf]) } ->
+                    (   { atom(A), current_op(_, _, F1) } ->
+                        "(", print_(A, Settings, Ps, '(', _), ")",
+                        print_(F, Settings, Ps, ')', L)
+                    ;   { compound(A),
+                          N1 >= 1, N1 =< 2,
+                          current_op(P1, S1, F1),
+                          spec_arity(S1, N1),
+                          ( Spec = yf, P < P1 -> true
+                          ; Spec = xf, P =< P1
+                          ) } ->
+                        "(", print_(A, Settings, Ps, '(', _), ") ",
+                        print_(F, Settings, Ps, ')', L)
+                    ;   print_(A, Settings, Ps, L0, L1),
+                        print_(F, Settings, Ps, L1, L)
+                    )
+                )
+            ;   { N =:= 2, current_op(P, Spec, F), spec_arity(Spec, N) } ->
+                (   { [A, B] = Args,
+                      ( nonvar(A), functor(A, F1, N1) -> true ; true),
+                      ( nonvar(B), functor(B, F2, N2) -> true ; true) },
+                    (   { atom(A), current_op(_, _, F1) } ->
+                        { L1 = ')' },
+                        "(", print_(A, Settings, Ps, '(', _), ")"
+                    ;   { compound(A),
+                          N1 >= 1, N1 =< 2,
+                          current_op(P1, S1, F1),
+                          spec_arity(S1, N1),
+                          ( Spec = yfx, P < P1 ->
+                            true
+                          ; ( Spec = xfx -> true ; Spec = xfy ), P =< P1
+                          ) } ->
+                        { L1 = ')' },
+                        "(", print_(A, Settings, Ps, '(', _), ")"
+                    ;   print_(A, Settings, Ps, L0, L1)
+                    ),
+                    (   { F = (',') } ->
+                        { L2 = (',') },
+                        ","
+                    /*
+                    ;   { member(F, [., :, /]) } ->
+                        print_(F, Settings, Ps, L1, L2)
+                    ;   { member(F, [-, +, *, /, **, ^, //, <<, >>, ..]) } ->
+                        print_(F, Settings, Ps, L1, L2)
+                    ;   { L2 = ' ' },
+                        " ", print_(F, Settings, Ps, ' ', _), " "
+                    % */
+                    ;   { false, member(F, [=]) } ->
+                        { L2 = ' ' },
+                        " ", print_(F, Settings, Ps, ' ', _), " "
+                    ;   print_(F, Settings, Ps, L1, L2)
+                    ),
+                    (   { atom(B), current_op(_, _, F2) } ->
+                        { L = ')' },
+                        "(", print_(B, Settings, Ps, '(', _), ")"
+                    ;   { compound(B),
+                          N2 >= 1, N2 =< 2,
+                          current_op(P2, S2, F2),
+                          spec_arity(S2, N2),
+                          ( Spec = xfy, P < P2 ->
+                            true
+                          ; ( Spec = xfx -> true ; Spec = yfx ), P =< P2
+                          ) } ->
+                        { L = ')' },
+                        "(", print_(B, Settings, Ps, '(', _), ")"
+                    ;   print_(B, Settings, Ps, L2, L)
+                    )
+                )
+            ;   { F = '.', N =:= 2 } ->
+                (   { ground(Compound),
+                      length(Compound, Cn),
+                      chars(Compound) } ->
+                    { L = '"' },
+                    {   (   MaxDepth > 0 ->
+                            (   Cn > MaxDepth ->
+                                Cut = true,
+                                Cn0 is max(MaxDepth - 4, 0),
+                                length(Cs, Cn0)
+                            ;   Cut = false,
+                                length(Cs, Cn)
+                            ),
+                            append(Cs, _, Compound)
+                        ;   Cs = Compound
+                        )
+                    },
+                    "\"",
+                    print_double_quoted(Cs),
+                    (   { Cut == true } ->
+                        " ..."
+                    ;   []
+                    ),
+                    "\""
+                ;   { L = ']' },
+                    "[", print_list(Compound, Settings, Ps, _), "]"
+                )
+            ;   { F = '{}', N =:= 1 } ->
+                { L = '}' },
+                "{", print_list(Args, Settings, Ps, _), "}"
+            ;   { L = ')' },
+                print_(F, Settings, Ps, L0, _),
+                "(", print_args(Args, Settings, Ps), ")"
+            )
+        )
+    ;   "..."
     ).
