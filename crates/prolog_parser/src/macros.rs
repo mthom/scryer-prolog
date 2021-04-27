061221b073698eba39095d79aee7879895f27b5b
panasenco <panasenco@users.noreply.github.com>
2021-04-27 11:58:39 -0700

    Alphabetized character logic in macros.rs and system_calls.rs

--- crates/prolog_parser/src/macros.rs
+++ crates/prolog_parser/src/macros.rs
@@ -5,131 +5,6 @@ macro_rules! char_class {
 }
 
 #[macro_export]
-macro_rules! symbolic_control_char {
-    ($c: expr) => {
-        $crate::char_class!($c, ['a', 'b', 'f', 'n', 'r', 't', 'v', '0'])
-    };
-}
-
-#[macro_export]
-macro_rules! space_char {
-    ($c: expr) => {
-        $c == ' '
-    };
-}
-
-#[macro_export]
-macro_rules! layout_char {
-    ($c: expr) => {
-        $crate::char_class!($c, [' ', '\n', '\t', '\u{0B}', '\u{0C}'])
-    };
-}
-
-#[macro_export]
-macro_rules! symbolic_hexadecimal_char {
-    ($c: expr) => {
-        $c == 'x'
-    };
-}
-
-#[macro_export]
-macro_rules! octal_digit_char {
-    ($c: expr) => {
-        ('0'..='7').contains(&$c)
-    };
-}
-
-#[macro_export]
-macro_rules! binary_digit_char {
-    ($c: expr) => {
-        $c >= '0' && $c <= '1'
-    };
-}
-
-#[macro_export]
-macro_rules! hexadecimal_digit_char {
-    ($c: expr) => {
-        ('0'..='9').contains(&$c) || ('A'..='F').contains(&$c) || ('a'..='f').contains(&$c)
-    };
-}
-
-#[macro_export]
-macro_rules! exponent_char {
-    ($c: expr) => {
-        $c == 'e' || $c == 'E'
-    };
-}
-
-#[macro_export]
-macro_rules! sign_char {
-    ($c: expr) => {
-        $c == '-' || $c == '+'
-    };
-}
-
-#[macro_export]
-macro_rules! new_line_char {
-    ($c: expr) => {
-        $c == '\n'
-    };
-}
-
-#[macro_export]
-macro_rules! end_line_comment_char {
-    ($c: expr) => {
-        $c == '%'
-    };
-}
-
-#[macro_export]
-macro_rules! comment_1_char {
-    ($c: expr) => {
-        $c == '/'
-    };
-}
-
-#[macro_export]
-macro_rules! comment_2_char {
-    ($c: expr) => {
-        $c == '*'
-    };
-}
-
-#[macro_export]
-macro_rules! capital_letter_char {
-    ($c: expr) => {
-        ('A'..='Z').contains(&$c)
-    };
-}
-
-#[macro_export]
-macro_rules! small_letter_char {
-    ($c: expr) => {
-        ('a'..='z').contains(&$c)
-    };
-}
-
-#[macro_export]
-macro_rules! variable_indicator_char {
-    ($c: expr) => {
-        $c == '_'
-    };
-}
-
-#[macro_export]
-macro_rules! graphic_char {
-    ($c: expr) => ($crate::char_class!($c, ['#', '$', '&', '*', '+', '-', '.', '/', ':',
-                                    '<', '=', '>', '?', '@', '^', '~']))
-}
-
-#[macro_export]
-macro_rules! graphic_token_char {
-    ($c: expr) => {
-        $crate::graphic_char!($c) || $crate::backslash_char!($c)
-    };
-}
-
-#[macro_export]
 macro_rules! alpha_char {
     ($c: expr) => {
         match $c {
@@ -158,6 +33,62 @@ macro_rules! alpha_char {
 }
 
 #[macro_export]
+macro_rules! alpha_numeric_char {
+    ($c: expr) => {
+        $crate::alpha_char!($c) || $crate::decimal_digit_char!($c)
+    };
+}
+
+#[macro_export]
+macro_rules! backslash_char {
+    ($c: expr) => {
+        $c == '\\'
+    };
+}
+
+#[macro_export]
+macro_rules! back_quote_char {
+    ($c: expr) => {
+        $c == '`'
+    };
+}
+
+#[macro_export]
+macro_rules! binary_digit_char {
+    ($c: expr) => {
+        $c >= '0' && $c <= '1'
+    };
+}
+
+#[macro_export]
+macro_rules! capital_letter_char {
+    ($c: expr) => {
+        ('A'..='Z').contains(&$c)
+    };
+}
+
+#[macro_export]
+macro_rules! comment_1_char {
+    ($c: expr) => {
+        $c == '/'
+    };
+}
+
+#[macro_export]
+macro_rules! comment_2_char {
+    ($c: expr) => {
+        $c == '*'
+    };
+}
+
+#[macro_export]
+macro_rules! cut_char {
+    ($c: expr) => {
+        $c == '!'
+    };
+}
+
+#[macro_export]
 macro_rules! decimal_digit_char {
     ($c: expr) => {
         ('0'..='9').contains(&$c)
@@ -172,41 +103,6 @@ macro_rules! decimal_point_char {
 }
 
 #[macro_export]
-macro_rules! alpha_numeric_char {
-    ($c: expr) => {
-        $crate::alpha_char!($c) || $crate::decimal_digit_char!($c)
-    };
-}
-
-#[macro_export]
-macro_rules! cut_char {
-    ($c: expr) => {
-        $c == '!'
-    };
-}
-
-#[macro_export]
-macro_rules! semicolon_char {
-    ($c: expr) => {
-        $c == ';'
-    };
-}
-
-#[macro_export]
-macro_rules! backslash_char {
-    ($c: expr) => {
-        $c == '\\'
-    };
-}
-
-#[macro_export]
-macro_rules! single_quote_char {
-    ($c: expr) => {
-        $c == '\''
-    };
-}
-
-#[macro_export]
 macro_rules! double_quote_char {
     ($c: expr) => {
         $c == '"'
@@ -214,9 +110,43 @@ macro_rules! double_quote_char {
 }
 
 #[macro_export]
-macro_rules! back_quote_char {
+macro_rules! end_line_comment_char {
     ($c: expr) => {
-        $c == '`'
+        $c == '%'
+    };
+}
+
+#[macro_export]
+macro_rules! exponent_char {
+    ($c: expr) => {
+        $c == 'e' || $c == 'E'
+    };
+}
+
+#[macro_export]
+macro_rules! graphic_char {
+    ($c: expr) => ($crate::char_class!($c, ['#', '$', '&', '*', '+', '-', '.', '/', ':',
+                                    '<', '=', '>', '?', '@', '^', '~']))
+}
+
+#[macro_export]
+macro_rules! graphic_token_char {
+    ($c: expr) => {
+        $crate::graphic_char!($c) || $crate::backslash_char!($c)
+    };
+}
+
+#[macro_export]
+macro_rules! hexadecimal_digit_char {
+    ($c: expr) => {
+        ('0'..='9').contains(&$c) || ('A'..='F').contains(&$c) || ('a'..='f').contains(&$c)
+    };
+}
+
+#[macro_export]
+macro_rules! layout_char {
+    ($c: expr) => {
+        $crate::char_class!($c, [' ', '\n', '\t', '\u{0B}', '\u{0C}'])
     };
 }
 
@@ -228,9 +158,23 @@ macro_rules! meta_char {
 }
 
 #[macro_export]
-macro_rules! solo_char {
+macro_rules! new_line_char {
     ($c: expr) => {
-        $crate::char_class!($c, ['!', '(', ')', ',', ';', '[', ']', '{', '}', '|', '%'])
+        $c == '\n'
+    };
+}
+
+#[macro_export]
+macro_rules! octal_digit_char {
+    ($c: expr) => {
+        ('0'..='7').contains(&$c)
+    };
+}
+
+#[macro_export]
+macro_rules! octet_char {
+    ($c: expr) => {
+        ('\u{0000}'..='\u{00FF}').contains(&$c)
     };
 }
 
@@ -242,5 +186,68 @@ macro_rules! prolog_char {
             || $crate::solo_char!($c)
             || $crate::layout_char!($c)
             || $crate::meta_char!($c)
+    };
+}
+
+#[macro_export]
+macro_rules! semicolon_char {
+    ($c: expr) => {
+        $c == ';'
+    };
+}
+
+#[macro_export]
+macro_rules! sign_char {
+    ($c: expr) => {
+        $c == '-' || $c == '+'
+    };
+}
+
+#[macro_export]
+macro_rules! single_quote_char {
+    ($c: expr) => {
+        $c == '\''
+    };
+}
+
+#[macro_export]
+macro_rules! small_letter_char {
+    ($c: expr) => {
+        ('a'..='z').contains(&$c)
+    };
+}
+
+#[macro_export]
+macro_rules! solo_char {
+    ($c: expr) => {
+        $crate::char_class!($c, ['!', '(', ')', ',', ';', '[', ']', '{', '}', '|', '%'])
+    };
+}
+
+#[macro_export]
+macro_rules! space_char {
+    ($c: expr) => {
+        $c == ' '
+    };
+}
+
+#[macro_export]
+macro_rules! symbolic_control_char {
+    ($c: expr) => {
+        $crate::char_class!($c, ['a', 'b', 'f', 'n', 'r', 't', 'v', '0'])
+    };
+}
+
+#[macro_export]
+macro_rules! symbolic_hexadecimal_char {
+    ($c: expr) => {
+        $c == 'x'
+    };
+}
+
+#[macro_export]
+macro_rules! variable_indicator_char {
+    ($c: expr) => {
+        $c == '_'
     };
 }
