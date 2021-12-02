bee5f329f3a0cdca4781ca1b9aa63f939989b16d
webstrand <webstrand@gmail.com>
2021-12-02 14:37:27 -0500

    use `-->` to define DCG nonterminals

--- src/lib/debug.pl
+++ src/lib/debug.pl
@@ -7,16 +7,17 @@
     (*)/1,
     ($)/1,
     ($-)/1,
-    (*)/3,
     ($)/3,
     ($-)/3
 ]).
 
+:- use_module(library(dcgs), [phrase/3]).
 :- use_module(library(format), [portray_clause/1]).
+:- use_module(library(lambda), [(\)/3,(^)/3,(^)/4]).
 
-:- meta_predicate *(0).
-:- meta_predicate $(0).
-:- meta_predicate $-(0).
+:- meta_predicate(*(0)).
+:- meta_predicate($(0)).
+:- meta_predicate($-(0)).
 
 $-(G_0) :-
    catch(G_0, Ex, ( portray_clause(exception:Ex:G_0), throw(Ex) ) ).
@@ -28,17 +29,17 @@ $(G_0) :-
 
 *(_).
 
-% Predicates adapted for use inside DCGs
-:- meta_predicate *(0, ?, ?).
-:- meta_predicate $(0, ?, ?).
-:- meta_predicate $-(0, ?, ?).
+% Equivalent DCG rules
+:- meta_predicate($(2, ?, ?)).
+:- meta_predicate($-(2, ?, ?)).
 
-$-(G_0, A, B) :-
-   catch(call(G_0, A, B), Ex, ( portray_clause(exception:Ex:G_0=(A,B)), throw(Ex) ) ).
+$-G_2 --> \A^B^
+   catch(phrase(G_2, A, B), Ex, (
+      portray_clause(exception:Ex:G_2=(A, B)), throw(Ex)
+   )).
 
-$(G_0, A, B) :-
-   portray_clause(call:G_0=(A,B)),
-   $-(G_0, A, B),
-   portray_clause(exit:G_0=(A,B)).
-
-*(_, A, A).
+$G_2 --> \A^B^(
+   portray_clause(call:G_2=(A, B)),
+   phrase($-G_2, A, B),
+   portray_clause(exit:G_2=(A, B))
+).
