21c36880f18b1687887fc56a7fd21cfd15e078d2
Nicolas Luck <nicolas@lucksus.eu>
2023-08-02 14:51:41 +0200

    Fix build

--- src/bin/scryer-prolog.rs
+++ src/bin/scryer-prolog.rs
@@ -1,6 +1,7 @@
 fn main() {
     use std::sync::atomic::Ordering;
     use scryer_prolog::*;
+    use scryer_prolog::atom_table::Atom;
 
     ctrlc::set_handler(move || {
         scryer_prolog::machine::INTERRUPT.store(true, Ordering::Relaxed);
