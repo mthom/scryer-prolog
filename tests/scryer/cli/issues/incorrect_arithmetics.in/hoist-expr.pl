720094c7218ec561d58dafc6f38fdd4dd4ad9e78
Aleksy Grabowski <hurufu@gmail.com>
2024-07-27 08:34:40 +0200

    Small fixes

--- tests/scryer/cli/issues/incorrect_arithmetics.in/hoist-expr.pl
+++ tests/scryer/cli/issues/incorrect_arithmetics.in/hoist-expr.pl
@@ -3,6 +3,24 @@
 :- use_module(library(format)).
 :- use_module(library(reif)).
 
+tt :-
+    tt(Clause),
+    nl, portray_clause(Clause), nl.
+tt((f(V is Expr) :- Sequence)) :-
+    sample("ttttffft", _ is Expr),
+    once(phrase(ae(Expr,Repl), Removed)),
+    append(Removed, [V is Repl], X),
+    list_sequence(X, Sequence).
+
+
+sample(Template, R) :-
+    setof(E, phrase(arith_relation(E), Template), Es),
+    length(Es, L),
+    repeat,
+    random_integer(0, L, I),
+    format("~n% Info: Selected ~dth out of ~d found aritmetic relations that satisfy ~s template:~n\t", [I,L,Template]),
+    nth0(I, Es, R).
+
 %% Proof-of-concept arithmetic expression expander
 %
 % TODO: Don't use DCG's, because they aren't available in loader.pl
@@ -14,14 +32,13 @@ test((f(V is Expr) :- Sequence)) :-
     append(Removed, [V is Repl], X),
     list_sequence(X, Sequence).
 
-ae(A, R) --> ex(A, R).
-ae(F, FR) --> ey(F, A, R, FR), ae(A, R).
 ae(F, FR) --> ez(F, A, B, AR, BR, FR), ae(A, AR), ae(B, BR).
+ae(F, FR) --> ey(F, A, R, FR), ae(A, R).
+ae(A, R) --> ex(A, R).
 
-ex([]         , R      ) --> [R = []].
-ex([H|T]      , R      ) --> [R = [H|T]].
-ex(zeta(A)    , R      ) --> [R = zeta(A)].
+ex(A          , R      ) --> [R = A], { nonvar(A), \+number(A), A \= e, A \= pi}.
 ex(A          , A      ) --> t, { var(A) }.
+ex(A          , A      ) --> t, { number(A) }.
 ex(e          , e      ) --> t.
 ex(pi         , pi     ) --> t.
 
@@ -35,7 +52,7 @@ ez(A*B,A,B, AR,BR,AR*BR) --> t.
 ez(A/B,A,B, AR,BR,AR/BR) --> t.
 ez(A^B,A,B, AR,BR,AR^BR) --> t.
 
-t --> [true].
+t --> [].
 
 %% list_sequence(List, Sequence).
 %
