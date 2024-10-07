43691eb409eb17bd50211919f446ba0440ecd523
Aleksy Grabowski <hurufu@gmail.com>
2024-10-07 08:04:41 +0200

    Reorganize code

--- src/lib/warnings.pl
+++ src/lib/warnings.pl
@@ -3,9 +3,9 @@
 :- use_module(library(format)).
 :- use_module(library(pio)).
 
-warn_fail(Format, Vars) :-
-    warn_fail(user_error, Format, Vars).
-warn_fail(Stream, Format, Vars) :-
+warn(Format, Vars) :-
+    warn(user_error, Format, Vars).
+warn(Stream, Format, Vars) :-
     prolog_load_context(file, File),
     prolog_load_context(term_position, position_and_lines_read(_,Line)),
     phrase_to_stream(
@@ -13,8 +13,7 @@ warn_fail(Stream, Format, Vars) :-
             "% Warning: ", format_(Format,Vars), format_(" at line ~d of ~a~n",[Line,File])
         ),
         Stream
-    ),
-    false.
+    ).
 
 % FIXME: Replace with predicate_property(_, built_in) when #2600 will be ready
 builtin((_;_)).
@@ -29,11 +28,9 @@ unsound_type_test(integer(_)).
 % example:
 %     x :- a. b, c.
 %
-user:term_expansion(G, _) :-
-    nonvar(G),
-    builtin(G),
-    functor(G, F, 2),
-    warn_fail("(~q) attempts to re-define ~w", [G, F/2]).
+term_warning(term, Term, "(~q) attempts to re-define ~w", [Term,F/2]) :-
+    builtin(Term),
+    functor(Term, F, 2).
 
 % Warn about unsound type test predicates and suggest using library(si).
 % Observe that following queries yield different results:
@@ -43,16 +40,23 @@ user:term_expansion(G, _) :-
 %     ?- integer(X), X=1.
 %        false.
 %
-user:goal_expansion(G, _) :-
-    nonvar(G),
-    unsound_type_test(G),
-    functor(G, F, 1),
-    warn_fail("~q is a constant source of wrong results, use ~a_si/1 from library(si)", [F/1,F]).
+term_warning(goal, Term, "~q is a constant source of wrong results, use ~a_si/1 from library(si)", [F/1,F]) :-
+    unsound_type_test(Term),
+    functor(Term, F, 1).
 
 % Warn when more than 2 negations are nested. Double negation has legit
 % use-case, but I don't think that more nested negations are ever useful.
 %
-user:goal_expansion(G, _) :-
-    nonvar(G),
-    G = (\+ \+ \+ _),
-    warn_fail("Nested negations can be reduced", []).
+term_warning(goal, \+ \+ \+_, "Nested negations can be reduced", []).
+
+expansion_warning(ExpansionKind, Term) :-
+    nonvar(Term),
+    term_warning(ExpansionKind, Term, Msg, Vars),
+    warn(Msg, Vars),
+    false.
+
+user:term_expansion(Term, _) :-
+    expansion_warning(term, Term).
+
+user:goal_expansion(Term, _) :-
+    expansion_warning(goal, Term).
