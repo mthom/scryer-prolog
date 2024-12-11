eef9f74a68a21af4a9572cf8bcc4057d8073bd0a
Jason Hemann <jason.hemann@gmail.com>
2024-12-10 21:18:09 -0500

    Documentation for read/2
    Mirrored the wording in the docstrings for `read/1` and `read_term/3`.

--- src/lib/builtins.pl
+++ src/lib/builtins.pl
@@ -766,6 +766,10 @@ read(Term) :-
     read_term(Stream, Term, []).
     % read(Stream, Term).
 
+%% read(+Stream, -Term).
+%
+% Read Term from the stream Stream with default options. **NOTE** This is not a general predicate
+% to read input from a file or the user. Use other predicates like `phrase_from_file/2` for that.
 read(Stream, Term) :-
     read_term(Stream, Term, []).
 
