777e6de9ee5b3b83b3022693290f867916f3c5c4
Skgland <bennet.blessmann+github@googlemail.com>
2025-09-28 13:43:09 +0200

    fix missing quotes

--- tests/scryer/cli/issues/ffi_alloc_mismatched_args.in/input.pl
+++ tests/scryer/cli/issues/ffi_alloc_mismatched_args.in/input.pl
@@ -4,4 +4,4 @@
 test :- ffi:array_type(u8, 2, Type), ffi:allocate(rust, Type, [Type, 0], _ArrayPtr).
 
 ?- test.
-   error(existence_error(ffi_struct_constructor,$[u8;2]/1),$ffi_allocate/4).
+   error(existence_error(ffi_struct_constructor,'$[u8;2]'/1),'$ffi_allocate'/4).
