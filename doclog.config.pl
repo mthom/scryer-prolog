f127d7a91c4e86f6656067a7e00e6e67754584bf
Doug <doug@ransom.vip>
2025-11-15 05:03:35 -0800

    removed ->

--- doclog.config.pl
+++ doclog.config.pl
@@ -5,7 +5,6 @@ websource("https://github.com/mthom/scryer-prolog/tree/master/src/lib").
 omit(["ops_and_meta_predicates.pl", "tabling"]).
 learn_pages_source_folder("learn").
 learn_pages_categories(["First steps", "Tutorials"]).
-reif_learn_pages_categories( ["First steps", "Tutorials"]).
 learn_pages([
 		   page("Let's play Brisca", "Tutorials", "lets-play-brisca.dj"),
 		   page("Test page", "First steps", "test-page.dj"),
