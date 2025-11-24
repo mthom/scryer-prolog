a639bc6aa524af531a4ee97b0b7d7b15479d14e0
Doug Ransom <doug@ransom.vip>
2025-11-24 13:14:23 -0800

    introduction to scryer first draft

---
+++ learn/getting-started/colon.pl
@@ -0,0 +1,4 @@
+a(3) :-  3#>4.
+deliberate syntax error
+a(2) : true.
+a(_) :-  X#>12.
---
+++ learn/getting-started/hello.pl
@@ -0,0 +1,8 @@
+:- use_module(library(format)).
+:- initialization(main).
+
+main :-
+    format("Hello, world!~n", []).
+
+hello_prolog :-
+    format("Hello, Prolog world!~n", []).
