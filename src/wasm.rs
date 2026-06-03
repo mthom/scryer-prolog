c2066a8e395b63c871917db30522ddbdc5d3c975
Alexander McLin <alex.mclin@gmail.com>
2026-06-03 17:57:10 -0400

    Issue 3223: trying again repair for the failing wasm32 build
    Removed `mut` modifier completely.

--- src/wasm.rs
+++ src/wasm.rs
@@ -129,7 +129,7 @@ impl WasmQueryState {
         let mut error = None;
         let mut to_drop = false;
         match &mut self.inner {
-            Some(mut inner) => inner.with_query_state_mut(|query_state| match query_state.next() {
+            Some(inner) => inner.with_query_state_mut(|query_state| match query_state.next() {
                 Some(Ok(leaf_answer)) => {
                     js_sys::Reflect::set(&ret, &"value".into(), &leaf_answer.into()).unwrap();
                     js_sys::Reflect::set(&ret, &"done".into(), &false.into()).unwrap();
